327bc10b08db0d051c786e9c5877ec3a5bc33ef1
taraswww777 <taraswww777@mail.ru>
2023-05-12 19:49:29 +0300

    make core part calc

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -11,10 +11,10 @@ export const Calculator = () => {
       margin: '0 auto',
     }}>
       <Layout.Content>
-        <Col span={24}>
-          <Input size={'large'} value={3} disabled />
-        </Col>
-        <Row>
+        <Row gutter={[0, 8]}>
+          <Col span={24}>
+            <Input size={'large'} value={3} disabled />
+          </Col>
           <Col span={20}>
             <Row gutter={[8, 8]}>
               {numberActions.map(n => (
