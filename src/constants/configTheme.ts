06f2c875f0ff96cb0ad9c35c0a2650d908e33eb6
taraswww777 <taraswww777@mail.ru>
2023-05-12 19:18:43 +0300

    fix configTheme

--- src/constants/configTheme.ts
+++ src/constants/configTheme.ts
@@ -1,7 +1,5 @@
-import {ThemeConfig} from 'antd';
+import {theme, ThemeConfig} from 'antd';
 
 export const ANT_THEME_CONFIG: ThemeConfig = {
-  token: {
-    colorPrimary: '#00b96b',
-  },
+  algorithm: theme.darkAlgorithm,
 }
