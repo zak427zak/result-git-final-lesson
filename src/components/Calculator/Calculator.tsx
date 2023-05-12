6fbd2b0a4ed32128f6f8dce12c70e639c273826c
taraswww777 <taraswww777@mail.ru>
2023-05-12 19:59:55 +0300

    add history

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,9 +1,17 @@
-import {Button, Col, Input, Layout, Row} from 'antd';
+import {Button, Col, Input, Layout, Row, Typography} from 'antd';
+import {useState} from 'react';
 
 export const Calculator = () => {
   const numberActions: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const mathActions: string[] = ['+', '-', '*', '/', '%'];
 
+  const [historyList, setHistoryList] = useState<string[]>([]);
+
+  const addHistoryItem = (historyItem: string) => setHistoryList([...historyList, historyItem]);
+
+  const goAction = (action: string | number) => {
+    addHistoryItem(`${action}`);
+  }
 
   return (
     <Layout style={{
@@ -13,13 +21,20 @@ export const Calculator = () => {
       <Layout.Content>
         <Row gutter={[0, 8]}>
           <Col span={24}>
+            <Typography>
+              <p>
+                {historyList.join(' ')}
+              </p>
+            </Typography>
+          </Col>
+          <Col span={24}>
             <Input size={'large'} value={3} disabled />
           </Col>
           <Col span={20}>
             <Row gutter={[8, 8]}>
               {numberActions.map(n => (
                 <Col span={7} key={n}>
-                  <Button shape={'circle'} size={'large'}>{n}</Button>
+                  <Button onClick={() => goAction(n)} shape={'circle'} size={'large'}>{n}</Button>
                 </Col>
               ))}
             </Row>
@@ -28,7 +43,7 @@ export const Calculator = () => {
             <Row gutter={[8, 8]}>
               {mathActions.map(n => (
                 <Col span={24} key={n}>
-                  <Button shape={'circle'} size={'large'}>{n}</Button>
+                  <Button onClick={() => goAction(n)} shape={'circle'} size={'large'}>{n}</Button>
                 </Col>
               ))}
             </Row>
